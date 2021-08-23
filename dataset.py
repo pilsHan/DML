aa5fdfcee16fce3520ce3728cddc04fd073246a6
Han Ji Su <87313780+pilsHan@users.noreply.github.com>
2021-08-23 16:06:01 +0900

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,12 +1,23 @@
-##
+##import
 import os
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
-##normalize
-cifar10_mean = (0.4914, 0.4822, 0.4465)
-cifar10_std = (0.2471, 0.2435, 0.2616)
-cifar100_mean = (0.5071, 0.4867, 0.4408)
-cifar100_std = (0.2675, 0.2565, 0.2761)
+
+##transform
+def trainsform(mean,std):
+    transform_train = transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomCrop(32,padding=4),
+            transforms.ToTensor(),
+            transforms.Normalize(mean,std)
+        ])
+    transform_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean,std)
+        ])
+
+    return transform_train,transform_test
+
 ##dateset
 def dataloader(args):
     if not os.path.exists(args.data_path):
@@ -14,36 +25,31 @@ def dataloader(args):
     dir_path = os.path.join(args.data_path,args.dataset)
     if not os.path.exists(dir_path):
         os.mkdir(dir_path)
+        
     if args.dataset == 'CIFAR10':
-        transform = transforms.Compose([
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomCrop(32,padding=4),
-            transforms.ToTensor(),
-            transforms.Normalize(cifar10_mean, cifar10_std)
-        ])
+        cifar10_mean = (0.4914, 0.4822, 0.4465)
+        cifar10_std = (0.2471, 0.2435, 0.2616)
+        transform_train,transform_test=trainsform(cifar10_mean,cifar10_std)
         train_loader = DataLoader(
-            datasets.CIFAR10(root=dir_path, train=True, transform=transform,download=args.download),
+            datasets.CIFAR10(root=dir_path, train=True, transform=transform_train,download=args.download),
             batch_size = args.BATCH_SIZE, shuffle = True, num_workers=args.num_workers
         )
         test_loader = DataLoader(
-            datasets.CIFAR10(root=dir_path, train=False, transform=transform),
-            batch_size = args.BATCH_SIZE, shuffle = True, num_workers=args.num_workers
+            datasets.CIFAR10(root=dir_path, train=False, transform=transform_test),
+            batch_size = args.BATCH_SIZE, shuffle = False, num_workers=args.num_workers
         )
         num_classes = 10
     elif args.dataset == 'CIFAR100':
-        transform = transforms.Compose([
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomCrop(32,padding=4),
-            transforms.ToTensor(),
-            transforms.Normalize(cifar100_mean, cifar100_std)
-        ])
+        cifar100_mean = (0.5071, 0.4867, 0.4408)
+        cifar100_std = (0.2675, 0.2565, 0.2761)
+        transform_train, transform_test = trainsform(cifar100_mean, cifar100_std)
         train_loader = DataLoader(
-            datasets.CIFAR100(root=dir_path, train=True, transform=transform,download=args.download),
+            datasets.CIFAR100(root=dir_path, train=True, transform=transform_train,download=args.download),
             batch_size = args.BATCH_SIZE, shuffle = True, num_workers=args.num_workers
         )
         test_loader = DataLoader(
-            datasets.CIFAR100(root=dir_path, train=False, transform=transform),
-            batch_size = args.BATCH_SIZE, shuffle = True, num_workers=args.num_workers
+            datasets.CIFAR100(root=dir_path, train=False, transform=transform_test),
+            batch_size = args.BATCH_SIZE, shuffle = False, num_workers=args.num_workers
         )
         num_classes = 100
 
